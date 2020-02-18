a26af11136a86256574a93c2eab46cac4b5e96a4
pranav805 <pranav.ige96@gmail.com>
2020-02-18 10:48:19 +0530

    ADDED: Checked validations for User's Email address

--- src/main/java/com/Bridgelabz/UserValidator.java
+++ src/main/java/com/Bridgelabz/UserValidator.java
@@ -5,7 +5,18 @@ public class UserValidator {
     public static void main(String[] args) {
         System.out.println("WELCOME TO USER REGISTRATION PROBLEM");
     }
-    private static final String NAME_PATTERN="^[A-Z]{1}[a-z]{2,}$";
+
+//    Pattern for First name and Last name
+    private final String NAME_PATTERN="^[A-Z]{1}[a-z]{2,}$";
+
+//    Pattern for EMail address
+    private final String EMAIL_PATTERN="^[A-Za-z]{3,}" +
+                                        "([.|+|_|-]?" +
+                                        "[A-Za-z0-9]+)?" +
+                                        "[@]" +
+                                        "[A-Za-z0-9]+" +
+                                        "[.][A-Za-z]{2,4}" +
+                                        "([.][A-Za-z]{2,4}+)?$";
 
     public boolean validateFirstName(String fName){
         return fName.matches(NAME_PATTERN);
@@ -13,5 +24,9 @@ public class UserValidator {
 
     public boolean validateLastName(String LName) {
         return LName.matches(NAME_PATTERN);
+    }
+
+    public boolean validateEMail(String EMail) {
+        return EMail.matches(EMAIL_PATTERN);
     }
 }
--- src/test/java/com/Bridgelabz/UserValidationTest.java
+++ src/test/java/com/Bridgelabz/UserValidationTest.java
@@ -4,32 +4,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserValidationTest {
+    UserValidator userValidator = new UserValidator();
 
-//    Test cases for First Name
+    //    Test cases for First Name
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("Pranav");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenAllLowerCaseProper_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("pranav");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenFirstName_WhenDigitOccurs_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("pranav805");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenFirstName_WhenAllUpperCase_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("PRANAV");
         Assert.assertEquals(false, result);
     }
@@ -37,28 +34,49 @@ public class UserValidationTest {
 //    Test cases for Last Name
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("Ige");
         Assert.assertEquals(true, result);
     }
     @Test
     public void givenLastName_WhenAllLowerCaseProper_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("ige");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenLastName_WhenAllUpperCase_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("IGE");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenLastName_WhenDigitOccurs_ShouldReturnFalse() {
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("Ige8054");
+        Assert.assertEquals(false, result);
+    }
+
+    //    Test cases for EMail
+    @Test
+    public void givenEMail_WhenProper_ShouldReturnTrue() {
+        boolean result = userValidator.validateEMail("pranav.ige96@gmail.co.in");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEMail_WhenSpacesOccur_ShouldReturnFalse() {
+        boolean result = userValidator.validateEMail("pranav ige96@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEMail_WhenAtSymbolIsMissing_ShouldReturnFalse() {
+        boolean result = userValidator.validateEMail("pranav.ige96gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEMail_WhenDigitsOccurInFirstPart_ShouldReturnFalse() {
+        boolean result = userValidator.validateEMail("pranav805.ige96@gmail.com");
         Assert.assertEquals(false, result);
     }
 }
