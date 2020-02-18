05ebf74ee36a4438fd8b3096ee1fe115613a075f
pranav805 <pranav.ige96@gmail.com>
2020-02-18 09:49:41 +0530

    ADDED: Checked validation for User's Last name

--- src/main/java/com/Bridgelabz/UserValidator.java
+++ src/main/java/com/Bridgelabz/UserValidator.java
@@ -1,10 +1,17 @@
 package com.Bridgelabz;
 
 public class UserValidator {
-    public boolean validateFirstName(String fName){
-        return fName.matches("^[A-Z]{1}[a-z]{2,}$");
-    }
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO USER REGISTRATION PROBLEM");
+    }
+    private static final String NAME_PATTERN="^[A-Z]{1}[a-z]{2,}$";
+
+    public boolean validateFirstName(String fName){
+        return fName.matches(NAME_PATTERN);
+    }
+
+    public boolean validateLastName(String LName) {
+        return LName.matches(NAME_PATTERN);
     }
 }
--- src/test/java/com/Bridgelabz/UserValidationTest.java
+++ src/test/java/com/Bridgelabz/UserValidationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class UserValidationTest {
 
+//    Test cases for First Name
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator userValidator = new UserValidator();
@@ -17,7 +18,6 @@ public class UserValidationTest {
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("pranav");
         Assert.assertEquals(false, result);
-
     }
 
     @Test
@@ -25,7 +25,6 @@ public class UserValidationTest {
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("pranav805");
         Assert.assertEquals(false, result);
-
     }
 
     @Test
@@ -33,6 +32,33 @@ public class UserValidationTest {
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateFirstName("PRANAV");
         Assert.assertEquals(false, result);
+    }
 
+//    Test cases for Last Name
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Ige");
+        Assert.assertEquals(true, result);
+    }
+    @Test
+    public void givenLastName_WhenAllLowerCaseProper_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("ige");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenLastName_WhenAllUpperCase_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("IGE");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenLastName_WhenDigitOccurs_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Ige8054");
+        Assert.assertEquals(false, result);
     }
 }
